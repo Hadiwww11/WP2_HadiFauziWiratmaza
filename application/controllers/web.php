72c9eea7c9e13afd84f6014e4172058dba6e2b26
hadifauziwiratmaza <hadisadut11@gmail.com>
2024-04-17 19:56:43 +0700

    Update terbaru Pertemuan 4

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -1,17 +1,27 @@
 <?php
-defined("BASEPATH") OR exit("No direct script access allowed");
+defined("BASEPATH") or exit("No direct script access allowed");
 
-class web extends CI_Controller {
+class web extends CI_Controller
+{
 
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
-        $this->load->helper('url');
+        // $this->load->helper('url');
     }
 
-    public function index() {
+    public function index()
+    {
         $data['judul'] = "Halaman Depan";
         $this->load->view('v_header', $data);
         $this->load->view('v_index', $data);
         $this->load->view('v_footer', $data);
-    }    
+    }
+    public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
 }
